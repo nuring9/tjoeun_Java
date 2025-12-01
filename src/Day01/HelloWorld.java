f0dd4cbbd54db8e8b38cf1614cacc03a6c9f23e2
박누리 <for9595@gmail.com>
2025-12-01 17:55:04 +0900

    12월1일 수업 첫날

---
+++ src/Day01/HelloWorld.java
@@ -0,0 +1,13 @@
+package Day01;
+
+public class HelloWorld {
+    static void main() {
+        System.out.println("hello world!");
+        System.out.print("hello");
+        System.out.print('J');
+        /*
+        여러 줄 주석 처리
+         */
+        // 한 줄 주석 처리
+    }
+}
---
+++ src/Day01/Printf.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class Printf {
+    static void main() {
+        System.out.printf("안녕하세요. %s 입니다.\n", "홍길동");
+        System.out.printf("네 나이는 %d 입니다.\n", 15);
+        System.out.printf("값 : %f 입니다.\n", 13.7);
+
+    }
+}
